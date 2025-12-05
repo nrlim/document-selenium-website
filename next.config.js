db540fb328f14f55a8f6fab9718f3d58610a01f7
Nuralim <nuralim@rds.co.id>
2025-12-05 16:12:18 +0700

    Disable SWC minification

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false,
+  experimental: {
+    turbopack: false,
+  }
 }
 
 module.exports = nextConfig
